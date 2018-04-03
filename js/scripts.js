3fe80a1db6aa8ef28c8e0198b7e5eb39a88c6b2f
andrew lupton and Noah Medoff <andrew@>
2018-04-03 11:20:50 -0700

    create functionality on webpage itself to show users balance. let user deposit or withdraw and it shows the new balance

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-function BankAccount(userName, accountNumber, balance){
+function BankAccount(userName, balance){
   this.userName = userName;
-  this.accountNumber = accountNumber;
   this.balance = balance;
 }
 
@@ -12,7 +11,37 @@ BankAccount.prototype.withdrawal = function(num){
   return this.balance -= num;
 };
 
+var newAccount;
+
 $(document).ready(function( ) {
 
 
+  $("form#nameGroup").submit(function(event) {
+    event.preventDefault();
+
+    var nameInput = $("#nameInput").val();
+    var initialDeposit = parseFloat($("#depoInput").val());
+
+    newAccount = new BankAccount(nameInput, initialDeposit);
+    console.log(newAccount);
+
+    $("#balance").text(newAccount.balance)
+
+  });
+
+  $("#depositSubmit").click(function(){
+    var depositInput = parseFloat($("#depositInput").val())
+
+    newAccount.deposit(depositInput);
+    console.log(newAccount);
+    $("#balance").text(newAccount.balance)
+  });
+
+  $("#withdrawalSubmit").click(function(){
+    var withdrawalInput = parseFloat($("#withdrawalInput").val())
+
+    newAccount.withdrawal(withdrawalInput);
+    console.log(newAccount);
+    $("#balance").text(newAccount.balance)
+  });
 });
